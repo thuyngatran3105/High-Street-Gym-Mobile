1b19392517d96439d9f23f9958d4049c7aac202e
thuyngatran3105 <tthuynga2005@gmail.com>
2025-06-10 06:41:00 +1000

    Initial commit - add project files

---
+++ backend/src/server.js
@@ -0,0 +1,69 @@
+import express from "express"
+import fileUpload from "express-fileupload"
+import cors from "cors"
+// import classController from "./controllers/classes.js"
+// import activityController from "./controllers/activities.js"
+// import userController from "./controllers/users.js"
+// import postController from "./controllers/blog_posts.js"
+// import bookingController from "./controllers/bookings.js"
+import userAPIRouter from "./routes/APIUsers.js"
+import authAPIRouter from "./routes/APIAuth.js"
+import classAPIRouter from "./routes/APIClasses.js"
+import blogAPIRouter from "./routes/APIBlogs.js"
+import bookingAPIRouter from "./routes/APIBookings.js"
+import activityAPIRouter from "./routes/APIActivities.js"
+import locationAPIRouter from "./routes/APILocations.js"
+
+const app = express()
+const port = 8080
+
+// Setup cors
+app.use(cors({
+    origin: true
+}))
+
+// Add middleware
+// app.use(session({
+//     secret: "secret phrase",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { secure: false }
+// }))
+
+// Enable the ejs view engine
+app.set("view engine", "ejs")
+
+// Enable support for URL-encoded request bodies (POST-ed form data)
+app.use(express.urlencoded({
+    extended: true,
+}))
+
+// Enable support for JSON body parsing
+app.use(express.json())
+
+// Enable file upload support
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+}))
+
+// Use the controllers
+// app.use(activityController)
+// app.use(classController)
+// app.use(userController)
+// app.use(postController)
+// app.use(bookingController)
+
+// Use each API controller with /api path segment
+app.use("/api/users", userAPIRouter)
+app.use("/api/auth", authAPIRouter)
+app.use("/api/classes", classAPIRouter)
+app.use("/api/blogs", blogAPIRouter)
+app.use("/api/bookings", bookingAPIRouter)
+app.use("/api/activities", activityAPIRouter)
+app.use("/api/locations", locationAPIRouter)
+
+//TODO: Catch all for 500 and 404 errors
+
+app.listen(port, () => {
+    console.log("Express backend started on http://localhost:" + port)
+})
